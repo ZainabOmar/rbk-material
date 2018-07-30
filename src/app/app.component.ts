3141ef99e5c88665ea7bd35ef7a84fa56df042d8
zoaby <zoaby.am@gmail.com>
2018-07-30 21:33:40 +0300

    fix the home page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { WOW } from 'wowjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  wow = new WOW();
+
+  constructor() {
+    this.wow.init();
+  }
+
+
 }
